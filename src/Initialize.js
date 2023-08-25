bfbdf969d8306ec844960915832d45580323d1d2
Andreas G. Christensen <48524202+Andreas-DEV@users.noreply.github.com>
2023-08-25 10:18:37 +0200

    Initialize

--- src/Initialize.js
+++ src/Initialize.js
@@ -1,18 +1,12 @@
 export default class Initialize {
 
   constructor() {
+    
+    let test = false;
 
-    console.log("Initialize ready");
-
-    this.ninja = document.createElement("div");
-    this.ninja.id = "ninja";
-    document.body.appendChild(this.ninja);
-
-    let ninjaImg = document.createElement("img");
-    ninjaImg.src = "./assets/ninja.png";
-    this.ninja.appendChild(ninjaImg);
-
-
+    if (test != true) {
+      console.log('its true');
+    }
 
   } // END constructor
 } // END class
