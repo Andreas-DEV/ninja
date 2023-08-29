dc50af6ec320cca67fa50edec539a300f2b9d347
Andreas G. Christensen <48524202+Andreas-DEV@users.noreply.github.com>
2023-08-29 09:13:22 +0200

    update 2

--- src/ShowActors.js
+++ src/ShowActors.js
@@ -22,7 +22,33 @@ class ShowActor {
 
         });
 
+
         /* MENU END */
+
+        const cardContainer = document.createElement("section");
+        cardContainer.id = "cardContainer";
+        document.body.appendChild(cardContainer);
+
+        const card = document.createElement("div");
+        card.id = "card";
+        cardContainer.appendChild(card);
+
+        const childCardArray = ["headline", "imagecon", "content", "info"];
+        childCardArray.forEach((elementId, index) => {
+            const cardChild = document.createElement("div");
+            cardChild.id = elementId;
+            card.appendChild(cardChild);
+        });
+
+        const showImg = document.createElement("img");
+        showImg.id = "showImg";
+        showImg.src = "../assets/red-ninja.png";
+        document.querySelector("#imagecon").appendChild(showImg);
+
+
+        const footer = document.createElement("footer");
+        footer.id = "footer";
+        document.body.appendChild(footer);
 
     }
 
